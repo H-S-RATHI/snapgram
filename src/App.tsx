4d9d7179f71a5e6a617830fef4eb6b115c74acca
H-S-RATHI <goluu.000111@gmail.com>
2023-12-02 10:53:04 +0530

    Bottom bar is done and document Formated

--- src/App.tsx
+++ src/App.tsx
@@ -24,18 +24,18 @@ const App = () => {
                 {/* private routes */}
                 <Route element={<RootLayout />}>
                     <Route index element={<Home />} />
-                    <Route path="/explore" element={<Explore/>}/>
-                    <Route path="/saved" element={<Saved/>}/>
-                    <Route path="/all-users" element={<AllUsers/>}/>
-                    <Route path="/Create-post" element={<CreatePost/>}/>
-                    <Route path="/update-post/:id" element={<EditPost/>}/>
-                    <Route path="/posts/:id" element={<PostDetails/>}/>
-                    <Route path="/profile/:id" element={<Profile/>}/>
-                    <Route path="/update-profile/:id" element={<UpdateProfile/>}/>
+                    <Route path="/explore" element={<Explore />} />
+                    <Route path="/saved" element={<Saved />} />
+                    <Route path="/all-users" element={<AllUsers />} />
+                    <Route path="/Create-post" element={<CreatePost />} />
+                    <Route path="/update-post/:id" element={<EditPost />} />
+                    <Route path="/posts/:id" element={<PostDetails />} />
+                    <Route path="/profile/:id" element={<Profile />} />
+                    <Route path="/update-profile/:id" element={<UpdateProfile />} />
                 </Route>
 
             </Routes>
-            <Toaster/>
+            <Toaster />
         </main>
     )
 }
--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -5,9 +5,9 @@ import { useEffect } from 'react';
 import { useUserContext } from '@/context/AuthContext';
 
 const Tobar = () => {
-  const {mutate: signOut, isSuccess} = useSignOutAccount();
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
   const navigate = useNavigate();
-  const {user} = useUserContext();
+  const { user } = useUserContext();
 
 
   useEffect(() => {
@@ -18,24 +18,24 @@ const Tobar = () => {
     <section className='topbar'>
       <div className="flex-between py-4 px-5">
         <Link to="/" className="flex gap-3 items-center">
-          <img 
-          src="/assets/images/logo.svg" 
-          alt="logo"
-          width={130}
-          height={325} />
+          <img
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={130}
+            height={325} />
         </Link>
 
         <div className="flex gap-4">
           <Button variant="ghost" className='shad-button_ghost' onClick={() => signOut()}>
-            <img 
-            src="/assets/icons/logout.svg" 
-            alt="logout" />
+            <img
+              src="/assets/icons/logout.svg"
+              alt="logout" />
           </Button>
           <Link to={`/profile/${user.id}`} className='flex-center gap-3'>
-            <img 
-            src={user.imageUrl || '/assets/images/profile-placeholder.svg'} 
-            alt="profile"
-            className='h-8 w-8 rounded-full' />
+            <img
+              src={user.imageUrl || '/assets/images/profile-placeholder.svg'}
+              alt="profile"
+              className='h-8 w-8 rounded-full' />
           </Link>
         </div>
 
